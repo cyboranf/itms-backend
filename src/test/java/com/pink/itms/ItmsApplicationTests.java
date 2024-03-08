0d97de0422dd06f4deef6973f4b7ef28800d325c
cyboranf <cyboranf@gmail.com>
2024-03-08 16:54:23 +0100

    init

---
+++ src/test/java/com/pink/itms/ItmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pink.itms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
