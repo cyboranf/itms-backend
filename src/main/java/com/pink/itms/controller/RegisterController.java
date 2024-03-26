6cd27a9cc22ceeb4e3d12a9df6247f2e5f541b31
cyboranf <cyboranf@gmail.com>
2024-03-26 12:08:35 +0100

    ITMS-70 Documentation for Sign in and Sign up feauter - added

--- src/main/java/com/pink/itms/controller/RegisterController.java
+++ src/main/java/com/pink/itms/controller/RegisterController.java
@@ -9,16 +9,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * Controller class for handling user registration.
+ */
 @RestController
 @RequestMapping("/api")
 public class RegisterController {
     private final RegisterService registerService;
-
+    /**
+     * Constructor for RegisterController.
+     *
+     * @param registerService the RegisterService instance to handle registration logic.
+     */
     public RegisterController(RegisterService registerService) {
         this.registerService = registerService;
     }
-
+    /**
+     * Endpoint to register a new user.
+     *
+     * @param registerRequestDTO the DTO containing registration details.
+     * @return ResponseEntity containing the response DTO with registration status.
+     */
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDTO> registerUser(@RequestBody RegisterRequestDTO registerRequestDTO) {
         RegisterResponseDTO registerResponseDTO = registerService.createAccount(registerRequestDTO);
