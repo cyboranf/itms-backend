a4803b40d44e9390a8bd51eb1ceca0d16ae92f2c
Karol66 <karolbal66@gmail.com>
2024-04-10 21:52:28 +0200

    ITMS-75 implementation of endpoint to create warehouse

--- src/main/java/com/pink/itms/service/WarehouseService.java
+++ src/main/java/com/pink/itms/service/WarehouseService.java
@@ -1,7 +1,9 @@
 package com.pink.itms.service;
 
+import com.pink.itms.dto.warehouse.WarehouseRequestDTO;
 import com.pink.itms.dto.warehouse.WarehouseResponseDTO;
 import com.pink.itms.mapper.WarehouseMapper;
+import com.pink.itms.model.Warehouse;
 import com.pink.itms.repository.WarehouseRepository;
 import com.pink.itms.validation.WarehouseValidator;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,19 @@ public class WarehouseService {
         this.warehouseRepository = warehouseRepository;
         this.warehouseMapper = warehouseMapper;
         this.warehouseValidator = warehouseValidator;
+    }
+
+    /**
+     * creates and saves warehouse entity
+     *
+     * @param warehouseRequestDTO warehouse to create
+     * @return {@link WarehouseResponseDTO} - response from created warehouse
+     */
+    public WarehouseResponseDTO createWarehouse(WarehouseRequestDTO warehouseRequestDTO) {
+        Warehouse warehouse = warehouseMapper.toEntity(warehouseRequestDTO);
+        Warehouse savedWarehouse = warehouseRepository.save(warehouse);
+
+        return warehouseMapper.toDTO(savedWarehouse);
     }
 
     /**
