edac9b461982b84622b356b8193ed9a85e5fc897
Karol66 <karolbal66@gmail.com>
2024-04-11 12:57:10 +0200

    ITMS-79 implementation of endpoint to edit warehouse

--- src/main/java/com/pink/itms/service/WarehouseService.java
+++ src/main/java/com/pink/itms/service/WarehouseService.java
@@ -1,15 +1,23 @@
 package com.pink.itms.service;
 
+import com.pink.itms.dto.taskType.TaskTypeRequestDTO;
+import com.pink.itms.dto.taskType.TaskTypeResponseDTO;
 import com.pink.itms.dto.warehouse.WarehouseRequestDTO;
 import com.pink.itms.dto.warehouse.WarehouseResponseDTO;
+import com.pink.itms.exception.taskType.ExistingNameException;
 import com.pink.itms.mapper.WarehouseMapper;
+import com.pink.itms.model.Product;
+import com.pink.itms.model.TaskType;
 import com.pink.itms.model.Warehouse;
+import com.pink.itms.repository.ProductRepository;
 import com.pink.itms.repository.WarehouseRepository;
 import com.pink.itms.validation.WarehouseValidator;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -40,7 +48,36 @@ public class WarehouseService {
     }
 
     /**
+     * Edits existing warehouse entity pointed by given id
+     *
+     * @param id                  of entity to edit
+     * @param warehouseRequestDTO values to replace with
+     * @return {@link WarehouseResponseDTO} - response from changed entity
+     */
+    @Transactional
+    public WarehouseResponseDTO editWarehouse(Long id, WarehouseRequestDTO warehouseRequestDTO) {
+        Optional<Warehouse> existingWarehouse = warehouseRepository.findById(id);
+
+        if (existingWarehouse.isEmpty()) {
+            throw new EntityNotFoundException("Warehouse with id " + id + " does not exist.");
+        }
+
+        Warehouse warehouse = existingWarehouse.get();
+        warehouse.setBuilding(warehouseRequestDTO.getBuilding());
+        warehouse.setZone(warehouseRequestDTO.getZone());
+        warehouse.setSpaceId(warehouseRequestDTO.getSpaceId());
+        warehouse.setSpaceHeight(warehouseRequestDTO.getSpaceHeight());
+        warehouse.setSpaceWidth(warehouseRequestDTO.getSpaceWidth());
+        warehouse.setSpaceLength(warehouseRequestDTO.getSpaceLength());
+
+        return warehouseMapper.toDTO(warehouse);
+    }
+
+
+
+    /**
      * Get all warehouses
+     *
      * @return List of WarehouseResponseDTO
      */
     public List<WarehouseResponseDTO> getAll() {
